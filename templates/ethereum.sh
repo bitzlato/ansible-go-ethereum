09f3bdbb246c7fab587bf18c89dcba02983719d4
mithrandir <mithrandir@bitzlato.com>
2022-07-20 11:19:08 +0300

    fixed service creation

--- templates/ethereum.sh
+++ templates/ethereum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-exec /home/ethereum/go-ethereum/build/bin/geth --{{ eth_network }} \
+exec {{ eth_root }}/go-ethereum/build/bin/geth --{{ eth_network }} \
   --datadir {{ eth_datadir }} --port={{ eth_port }} --maxpeers=200 \
   --http --http.addr={{ eth_http_addr}} --http.port={{ eth_http_port }} --http.api="eth,web3,net,personal" \
   --ws --ws.addr={{ eth_ws_addr }} --ws.port={{ eth_ws_port }}  --ws.api="eth,web3,net,personal" \
